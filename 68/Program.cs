d8c4f6557d2c4a30c1d350c15929260cb292c0a3
Maxim Sorokin <maximka773@mail.ru>
2022-12-02 23:36:40 +0300

    68 done

--- 68/Program.cs
+++ 68/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите метод вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите M: ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.Write("Введите N: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+Console.WriteLine(Ackermann(m, n));
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return ++n;
+    }
+    if (m > 0 & n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    if (m > 0 & n > 0)
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    return Ackermann(m, n);
+}

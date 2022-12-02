5181dfb9340ca8770375619d6d2cf9f756ba3c6d
Maxim Sorokin <maximka773@mail.ru>
2022-12-02 22:07:29 +0300

    64 done

---
+++ 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите метод, который выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите N: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+PrintNumbers(n);
+
+void PrintNumbers(int n, int i = 1)
+{
+    if (i > n)
+    {
+        return;
+    }
+    Console.Write($"{i} ");
+    PrintNumbers(n, i + 1);
+}

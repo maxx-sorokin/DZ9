8fde739b2c799e245811332534467f5b5d703637
Maxim Sorokin <maximka773@mail.ru>
2022-12-02 23:12:00 +0300

    66 done

--- 66/Program.cs
+++ 66/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите метод, который найдёт
+// сумму натуральных элементов в промежутке от M до N. Через рекурсию!
+
+
+Console.Write("Введите M: ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.Write("Введите N: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+if (m <= n)
+{
+    Console.WriteLine(SumNumbers(m, n));
+}
+else
+{
+    Console.WriteLine(SumNumbers(n, m));
+}
+
+int SumNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    return m + SumNumbers(++m, n);
+}
